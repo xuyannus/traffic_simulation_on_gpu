e53a81a46be73738ce0cdd1b9e1348eeda7042be
xuyan <xuyan.nus@gmail.com>
2014-02-26 01:26:48 +0800

    re-do experiment 1 and more importantly clarify the concept of road ID on CPU and road Index on GPU

--- traffic_simulation_on_gpu/shell_exp1.sh
+++ traffic_simulation_on_gpu/shell_exp1.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-for demand_level in 10000 50000
-do 
+#for demand_level in 10000 50000
+for demand_level in 50000
+do
 
 network_file_path="data/exp1_network/network_10_rank.dat"
 demand_file_path="data/exp1/demand_10_$demand_level.dat"
-simulation_output_file_path="output_exp1_again/simulated_outputs_mode0_$demand_level.txt"
+simulation_output_file_path="output_exp1/simulated_outputs_mode0_$demand_level.txt"
 
 echo "do serial code"
-./Debug/ETS_CPU_AND_GPU_Source_Code 1 $network_file_path $demand_file_path $simulation_output_file_path > ./output_exp1_again/console_$demand_level.txt
+./Debug/ETS_CPU_AND_GPU_Source_Code 1 $network_file_path $demand_file_path $simulation_output_file_path > ./output_exp1/console_$demand_level.txt
 echo "serial code done"
 
 
@@ -17,14 +18,17 @@ for EXECUTION_MODE in 2 4
 do
 
 for Partition_MODE in 2 4 6 8 10 20 30 40 60 121
+#for Partition_MODE in 2
 do
 
 network_file_path="data/exp1_network/network_10_rank.dat_$Partition_MODE"
-simulation_output_file_path="output_exp1_again/simulated_outputs_mode$EXECUTION_MODE.$demand_level.$Partition_MODE.txt"
+simulation_output_file_path="output_exp1/simulated_outputs_mode$EXECUTION_MODE.$demand_level.$Partition_MODE.txt"
 
-./Debug/ETS_CPU_AND_GPU_Source_Code $EXECUTION_MODE $network_file_path $demand_file_path $simulation_output_file_path > ./output_exp1_again/console_$Partition_MODE.$EXECUTION_MODE.$demand_level.txt
+echo "./Debug/ETS_CPU_AND_GPU_Source_Code $EXECUTION_MODE $network_file_path $demand_file_path $simulation_output_file_path"
 
-java -jar ./RMSNToolkit.jar output_exp1_again/simulated_outputs_mode0_$demand_level.txt $simulation_output_file_path > ./output_exp1_again/RMSN_$Partition_MODE.$EXECUTION_MODE.$demand_level.txt
+./Debug/ETS_CPU_AND_GPU_Source_Code $EXECUTION_MODE $network_file_path $demand_file_path $simulation_output_file_path > ./output_exp1/console_$Partition_MODE.$EXECUTION_MODE.$demand_level.txt
+
+java -jar ./RMSNToolkit.jar output_exp1/simulated_outputs_mode0_$demand_level.txt $simulation_output_file_path > ./output_exp1/RMSN_$Partition_MODE.$EXECUTION_MODE.$demand_level.txt
 
 done
 
